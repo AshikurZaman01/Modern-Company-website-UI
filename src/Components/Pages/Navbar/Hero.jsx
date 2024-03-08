a417fd4abfdfbbbde6ac29c5a7ab1663e3efa0d4
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-03-09 03:29:00 +0600

    add hero section

--- src/Components/Pages/Navbar/Hero.jsx
+++ src/Components/Pages/Navbar/Hero.jsx
@@ -6,10 +6,10 @@ const Hero = () => {
         <div className="min-h-screen flex gap-8 items-center bg-gradient-to-r from-violet-950 to-violet-900 pt-20">
 
             <section className="container flex flex-col items-center justify-between">
-                <div className="grid grid-cols-1 md:grid-cols-2 dark:text-white">
+                <div className="grid grid-cols-1 md:grid-cols-2 dark:text-white space-y-5">
 
                     {/* text */}
-                    <div className='flex flex-col items-center gap-4 md:items-start md:text-left md:justify-center space-y-4'>
+                    <div className='flex flex-col items-center gap-4 md:items-start md:text-left md:justify-center space-y-4 order-2 md:order-1 mb-0'>
                         <h1 className='text-4xl'>We Build Apps That Get <span className='text-green-500'>Trending On</span> <span className='text-pink-700'>Appworld</span></h1>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi cupiditate, nostrum veritatis commodi distinctio debitis nam repudiandae vitae.</p>
 
@@ -21,7 +21,7 @@ const Hero = () => {
                     {/* text */}
 
                     {/* image */}
-                    <div className='mx-auto max-w-xs p-4'>
+                    <div className='mx-auto max-w-xs p-4 order-1 md:order-2'>
                         <img src={heroImg} alt="" className="mx-auto" />
                     </div>
                     {/* image */}
