fd16b2d9ff0fc63e2f683354867aac183d4d635f
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-03-09 03:37:13 +0600

    add AOS animation in Hero Section

--- src/Components/Pages/Navbar/Hero.jsx
+++ src/Components/Pages/Navbar/Hero.jsx
@@ -1,7 +1,14 @@
+import { useEffect } from 'react';
 import heroImg from '../../../assets/undraw_real_time_sync_re_nky7.svg';
-import Aos from 'aos';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const Hero = () => {
+
+    useEffect(() => {
+        AOS.init();
+    }, []);
+
     return (
         <div className="min-h-screen flex gap-8 items-center bg-gradient-to-r from-violet-950 to-violet-900 pt-20">
 
@@ -9,7 +16,12 @@ const Hero = () => {
                 <div className="grid grid-cols-1 md:grid-cols-2 dark:text-white space-y-5">
 
                     {/* text */}
-                    <div className='flex flex-col items-center gap-4 md:items-start md:text-left md:justify-center space-y-4 order-2 md:order-1 mb-0'>
+                    <div
+                        data-aos="fade-right"
+                        data-aos-duration="1000"
+                        data-aos-once="true"
+
+                        className='flex flex-col items-center gap-4 md:items-start md:text-left md:justify-center space-y-4 order-2 md:order-1 mb-0'>
                         <h1 className='text-4xl'>We Build Apps That Get <span className='text-green-500'>Trending On</span> <span className='text-pink-700'>Appworld</span></h1>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi cupiditate, nostrum veritatis commodi distinctio debitis nam repudiandae vitae.</p>
 
@@ -21,7 +33,11 @@ const Hero = () => {
                     {/* text */}
 
                     {/* image */}
-                    <div className='mx-auto max-w-xs p-4 order-1 md:order-2'>
+                    <div
+                        data-aos="fade-left"
+                        data-aos-duration="1000"
+                        data-aos-once="true"
+                        className='mx-auto max-w-xs p-4 order-1 md:order-2'>
                         <img src={heroImg} alt="" className="mx-auto" />
                     </div>
                     {/* image */}
