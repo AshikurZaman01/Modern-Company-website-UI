fc0f2dd82777d1f6868f94b9d1d5dd52d84665fd
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-03-09 01:42:39 +0600

    fixed darkmood

---
+++ src/Components/Pages/Navbar/Hero.jsx
@@ -0,0 +1,12 @@
+
+const Hero = () => {
+    return (
+        <div>
+            <section className="h-[650px] bg-white dark:bg-black">
+                d
+            </section>
+        </div>
+    );
+};
+
+export default Hero;
--- src/Components/Pages/Navbar/Navbar.jsx
+++ src/Components/Pages/Navbar/Navbar.jsx
@@ -1,30 +1,13 @@
 import { NavLink } from "react-router-dom";
 import { BiPhoneCall } from "react-icons/bi";
 import { FaMoon } from "react-icons/fa";
-import { FaSun } from "react-icons/fa6";
+import { FaSun } from "react-icons/fa";
 import { FaCaretDown } from "react-icons/fa";
 import { useEffect, useState } from "react";
 
 const Navbar = () => {
-
     const [theme, setTheme] = useState('dark');
     const element = document.documentElement;
-
-    useEffect(() => {
-        if (theme === 'dark') {
-            element.classList.add('dark');
-            localStorage.setItem('theme', 'dark');
-        } else {
-            element.classList.add('light');
-            localStorage.removeItem('theme');
-            localStorage.setItem('theme', 'light');
-        }
-    }, [element.classList, theme])
-
-    const toggleTheme = () => {
-        const newTheme = theme === 'dark' ? 'light' : 'dark';
-        setTheme(newTheme);
-    };
 
     useEffect(() => {
         const savedTheme = localStorage.getItem('theme');
@@ -33,28 +16,42 @@ const Navbar = () => {
         }
     }, []);
 
+    useEffect(() => {
+        if (theme === 'dark') {
+            element.classList.add('dark');
+            element.classList.remove('light');
+            localStorage.setItem('theme', 'dark');
+        } else {
+            element.classList.add('light');
+            element.classList.remove('dark');
+            localStorage.setItem('theme', 'light');
+        }
+    }, [theme, element.classList]);
+
+    const toggleTheme = () => {
+        const newTheme = theme === 'dark' ? 'light' : 'dark';
+        setTheme(newTheme);
+    };
+
     return (
         <div>
             <header className="bg-navbar border-b-[1px] border-primary/50">
                 <nav className="container flex justify-between items-center h-[70px] py-2 ">
-
                     {/* logo */}
                     <div className="text-2xl md:text-3xl text-white">
-                        <NavLink>Coz<span className="inline-block font-bold text-primary ">Web</span></NavLink>
+                        <NavLink to="/">Coz<span className="inline-block font-bold text-primary">Web</span></NavLink>
                     </div>
                     {/* logo */}
-
                     {/* desktop menu section */}
                     <div>
                         <ul className="flex items-center gap-4 text-white">
-
                             {/* home section */}
                             <li className="group">
-                                <NavLink className="flex items-center relative  cursor-pointer">Home
+                                <NavLink to="/" className="flex items-center relative  cursor-pointer">Home
                                     <span><FaCaretDown className="text-xl transition-all duration-200 group-hover:rotate-180" /></span>
                                 </NavLink>
                                 {/* home dropdown */}
-                                <div className="absolute  hidden z-[9999]  w-[150px] bg-white text-black group-hover:block rounded-md shadow-md">
+                                <div className="absolute hidden z-[9999]  w-[150px] bg-white text-black group-hover:block rounded-md shadow-md">
                                     <ul className="space-y-3">
                                         <li className="cursor-pointer hover:bg-violet-200 rounded-md p-2 m-1">Services</li>
                                         <li className="cursor-pointer hover:bg-violet-200 rounded-md p-2 m-1">About us</li>
@@ -64,23 +61,20 @@ const Navbar = () => {
                                 {/* home dropdown */}
                             </li>
                             {/* home section */}
-
                             {/* services  */}
                             <li className="group">
-                                <NavLink className="flex items-center relative  cursor-pointer">Services
+                                <NavLink to="/services" className="flex items-center relative  cursor-pointer">Services
                                     <span><FaCaretDown className="text-xl transition-all duration-200 group-hover:rotate-180" /></span>
                                 </NavLink>
                                 {/* services dropdown full wide */}
                                 <div className="absolute hidden rounded-b-3xl w-full z-[9999] left-0 top-[71px] p-2 bg-white text-black shadow-md group-hover:block">
                                     <div className="grid grid-cols-3 gap-5 ">
-
                                         <div className="overflow-hidden">
                                             <img className="max-h-[300px] w-full object-fill rounded-b-3xl " src="https://images.unsplash.com/photo-1709668158989-ca9f67bb9bb9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzOHx8fGVufDB8fHx8fA%3D%3D" alt="" />
                                         </div>
                                         <div className="col-span-2">
                                             <h1 className="text-2xl font-semibold">Best Selling</h1>
                                             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae omnis nostrum ratione maiores doloremque assumenda fugiat porro quod ducimus sint blanditiis, eius ipsam doloribus quas. Suscipit veniam unde sequi aperiam!</p>
-
                                             <div className="grid grid-cols-3 mt-5">
                                                 <div>
                                                     <h1 className="text-xl font-semibold">Development</h1>
@@ -103,21 +97,17 @@ const Navbar = () => {
                                                 </div>
                                             </div>
                                         </div>
-
                                     </div>
                                 </div>
                                 {/* services dropdown full wide */}
                             </li>
                             {/* services dropdown */}
-
-                            <li><NavLink>Contact Us</NavLink></li>
-
+                            <li><NavLink to="/contact">Contact Us</NavLink></li>
                             {/* call on us */}
                             <li className="flex items-center gap-4">
                                 <div>
                                     <BiPhoneCall className="text-2xl  h-[40px] w-[40px] bg-primary rounded-md p-2 hover:bg-primary/90" />
                                 </div>
-
                                 <div>
                                     <div>
                                         <p>Call us on</p>
@@ -125,24 +115,23 @@ const Navbar = () => {
                                             <a href="+91132423">+91 132423</a>
                                         </p>
                                     </div>
-
                                     <div>
-
                                     </div>
                                 </div>
                             </li>
                             {/* call on us */}
-
                             {/* dark and light mood */}
                             <li>
-                                <div className="text-2xl ml-5 cursor-pointer" onClick={toggleTheme}>
+                                <div className="text-2xl ml-5 cursor-pointer" >
                                     {
-                                        theme === 'dark' ? <FaMoon /> : <FaSun />
+                                        theme === 'dark' ?
+                                            <FaMoon onClick={toggleTheme} />
+                                            :
+                                            <FaSun onClick={toggleTheme} />
                                     }
                                 </div>
                             </li>
                             {/* dark and light mood */}
-
                         </ul>
                     </div>
                     {/* desktop menu section */}
